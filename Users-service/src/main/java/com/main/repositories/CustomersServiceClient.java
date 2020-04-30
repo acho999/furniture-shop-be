2de1f3f94f441208bbe24dc07036f213f9afa1d0
Angel Zahariev <angel1984zahariev@abv.bg>
2020-04-30 10:00:01 +0300

    Implement fallback method in users service

--- Users-service/src/main/java/com/main/repositories/CustomersServiceClient.java
+++ Users-service/src/main/java/com/main/repositories/CustomersServiceClient.java
@@ -1,10 +1,14 @@
 package com.main.repositories;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.main.configuration.CustomersServiceFallback;
-import com.main.configuration.CustomersServiceFallbackFactory;
+
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
 
 @FeignClient(name = "customers-service",fallback = CustomersServiceFallback.class)
 public interface CustomersServiceClient {
@@ -13,3 +17,43 @@ public interface CustomersServiceClient {
 	public void createCustomer();
 
 }
+
+@Component
+class CustomersServiceFallbackFactory implements FallbackFactory<CustomersServiceClient>{
+
+	@Override
+	public CustomersServiceClient create(Throwable cause) {
+		// TODO Auto-generated method stub
+		return new CustomersServiceFallback(cause);
+	}
+
+}
+
+@Component
+class CustomersServiceFallback implements CustomersServiceClient{
+	
+	private final Throwable cause;
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	public CustomersServiceFallback(Throwable cause) {
+		this.cause = cause;
+	}
+
+	@Override
+	public void createCustomer() {
+		
+		if (this.cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+			
+			this.logger.error("404 error took place when call createCustomer" + this.cause.getLocalizedMessage());
+			
+		} else {
+			 this.logger.error("Other error took place" + this.cause.getLocalizedMessage());
+		}
+		
+	}
+	
+}
+
+
+
