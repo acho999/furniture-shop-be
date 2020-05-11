c62e2c513688c82afba6ba071483ed016d268a82
Angel Zahariev <angel1984zahariev@abv.bg>
2020-05-11 11:38:02 +0300

    Clear imports

--- Authentication-service/src/main/java/com/main/security/AuthorizaionFilter.java
+++ Authentication-service/src/main/java/com/main/security/AuthorizaionFilter.java
@@ -11,14 +11,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
--- Authentication-service/src/main/java/com/main/security/SecurityConfig.java
+++ Authentication-service/src/main/java/com/main/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.main.security;
 
-import org.aspectj.weaver.patterns.ThisOrTargetAnnotationPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
